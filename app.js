1d3240b0181e63dd00646e98faabcaa3e54190ea
caglayan <caglayanustaa@gmail.com>
2024-09-24 21:33:00 +0300

    blog kodluyoruz

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require("mongoose")
 const ejs = require('ejs')
+const Post = require('./models/Post');
+
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db');
+
+
 
 const app = express();
 
@@ -7,11 +13,14 @@ app.set("view engine","ejs")
 
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-
-app.get('/', (req, res) => {
-
-  res.render('index')
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index',{
+    posts
+  })
 })
 app.get('/about', (req, res) => {
 
@@ -20,6 +29,10 @@ app.get('/about', (req, res) => {
 app.get('/add-post', (req, res) => {
 
   res.render('add_post')
+})
+app.post('/posts', async (req, res) => {
+  const post = await Post.create(req.body)
+  res.redirect('/')
 })
 
 const port = 3000;
